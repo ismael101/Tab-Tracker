552fad217b143ae7781dc70ec15eccad1b599929
IsmaelFarah101 <ismaelomermohamed1@gmail.com>
2019-07-16 05:51:09 -0500

    Added frontend Functionaloty and Created the server

---
+++ client/src/views/Register.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>Register Component</h1>
+        <input v-model='email' type='email' name='email' placeholder="Enter Email">
+        <input v-model='password' type='password' name='password' placeholder="Enter Password">
+        <button>Register</button>
+
+    </div>
+</template>
+
+<script>
+import Authentication from '../services/Authentication.js'
+export default {
+    data(){
+        return{
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        async register(){
+            const response = await Authentication.register({
+                email:this.email,
+                password:this.password
+            })
+            console.log(response.data)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
